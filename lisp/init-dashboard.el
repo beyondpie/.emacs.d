46d6b533350edbadd8fce5c8a8cb14efe2820ed1
beyondpie <zsp07@tsinghua.org.cn>
2020-12-17 15:35:11 -0500

    refactor dashboard

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -1,60 +1,39 @@
 ;;; init-dashboard.el --- Initialize dashboard configurations.	-*- lexical-binding: t -*-
 
 ;;; Commentary:
-;; Ref: seagle
 
 ;;; Code:
-(require 'init-const)
-;; optional for dashboard
-(require-package 'all-the-icons)
+
+(use-package all-the-icons
+  :ensure t
+  :pin melpa)
+
+(use-package page-break-lines
+  :ensure t
+  :pin melpa)
+
 ;; https://github.com/emacs-dashboard/emacs-dashboard
-(require-package 'dashboard)
-(dashboard-setup-startup-hook)
-
-(setq dashboard-banner-logo-title "Love at Boston.")
-(setq dashboard-startup-banner mybanner)
-
-;; show navigator below the banner
-(setq dashboard-set-navigator t)
-(setq dashboard-navigator-buttons
-      `(;; line1
-	((, (all-the-icons-octicon
-             "mark-github" :height 1.1 :v-adjust 0.0)
-	    "GithubPage" "Browse githubpage"
-	    (lambda (&rest _) (browse-url beyondpie-homepage)))
-	 )))
-
-;; Content is not centered by default. To center, set
-(setq dashboard-center-content t)
-
-;; To disable shortcut "jump" indicators for each section, set
-(setq dashboard-show-shortcuts nil)
-
-;; show items
-(setq dashboard-items '((recents  . 5)
-                        (bookmarks . 5)
-                        (projects . 5)))
-;; use icons to the widget
-(setq dashboard-set-heading-icons t)
-(setq dashboard-set-file-icons t)
-;; modify the icons from the all-the-icons
-(dashboard-modify-heading-icons '((recents . "file-text")
-                                  (bookmarks . "book")))
-
-;; show dashboard when using emacsclient -c
-(setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
-
-;; show packages loaded and init time
-(setq dashboard-set-init-info t)
-(setq dashboard-init-info "Emacs at your service.")
-
-;; close randome footnote
-;;(setq dashboard-set-footer nil)
-(setq dashboard-footer-messages '("吾志所向, 一往无前，愈挫愈奋，再接再厉。"))
-(setq dashboard-footer-icon (all-the-icons-octicon "dashboard"
-                                                   :height 1.1
-                                                   :v-adjust -0.05
-                                                   :face 'font-lock-keyword-face))
+(use-package dashboard
+  :ensure t
+  :pin melpa
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-banner-logo-title "Love at Harvard."
+        dashboard-startup-banner mybanner
+        dashboard-set-navigator t
+        dashboard-center-content nil
+        dashboard-show-shortcuts nil
+        dashboard-set-heading-icons nil
+        dashboard-set-file-icons nil
+        dashboard-set-init-info t
+        dashboard-init-info "前有古人，后有来者；行你所行，前无古人。"
+        dashboard-footer-messages '("吾志所向, 一往无前，愈挫愈奋，再接再厉。")
+        initial-buffer-choice (lambda () (get-buffer "*dashboard*"))
+        dashboard-items '((recents . 5)
+                          (bookmarks . 5)
+                          (projects . 5))
+        )
+  )
 
 (provide 'init-dashboard)
 ;;; init-dashboard.el ends here
