6377392e85f40d3b6c95b7fdb98b6ed00d9c751b
beyondpie <zsp07@tsinghua.org.cn>
2020-12-17 22:26:14 -0500

    set flycheck open manually

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -7,9 +7,20 @@
   :ensure t
   :pin melpa
   :hook (after-init . global-flycheck-mode)
+  :init
+  (setq flycheck-check-syntax-automatically nil)
   :config
   (setq flycheck-display-errors-function
         #'flycheck-display-error-messages-unless-error-list)
+  :general
+  (:states '(normal visual insert emacs)
+           :prefix beyondpie/normal-leader-key
+           :non-normal-prefix beyondpie/non-normal-leader-key
+           "eb" '(flycheck-buffer :which-key "flycheck-buffer")
+           "el" '(flycheck-list-errors :which-key "flycheck-list-errors")
+           "en" '(flycheck-next-error :which-key "flycheck-next-error")
+           "ep" '(flycheck-previous-error :which-key "flycheck-previous-error")
+           )
   )
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
