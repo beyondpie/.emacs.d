838f6c53f28e54eee95b38049d270e21cbce3468
beyondpie <zsp07@tsinghua.org.cn>
2021-01-14 11:18:47 -0500

    add straight for package installing.

--- init.el
+++ init.el
@@ -28,6 +28,20 @@
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
+;; use straght
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (require 'init-elpa)
 (require 'init-path)
 (require 'init-const)
