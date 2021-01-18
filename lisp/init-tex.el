8309f34f86680cc10fc219d50ef4b61a439e7b59
beyondpie <zsp07@tsinghua.org.cn>
2021-01-18 00:06:40 -0500

    add sroll keys like spacemacs in pdf-view-mode

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -82,6 +82,13 @@
   :config
   (pdf-tools-install)
   (setq auto-revert-interval 0.5)
+  :general
+  (:states '(normal)
+           :keymaps 'pdf-view-mode-map
+           "d" '(pdf-view-scroll-up-or-next-page :which-key "scroll-next")
+           "u" '(pdf-view-scroll-down-or-previous-page :which-key "scroll-back")
+           "b" '(pdf-view-scroll-down-or-previous-page :which-key "scroll-back")
+           )
   )
 
 
