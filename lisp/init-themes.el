9618b6558e47c9084b1ee465805ff5562c09d28a
beyondpie <zsp07@tsinghua.org.cn>
2021-03-05 22:11:59 -0500

    use modus theme as default

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -3,6 +3,9 @@
 ;;; Commentary:
 ;; spacemacs-theme setting
 ;; https://github.com/nashamri/spacemacs-theme/issues/42
+;; modus setting
+;; https://protesilaos.com/modus-themes
+
 
 ;;; Code:
 
@@ -10,11 +13,33 @@
   :ensure spacemacs-theme
   :pin melpa
   :defer t
-  :hook (after-init . (lambda ()
-                        (load-theme 'spacemacs-dark t)
-                        (add-to-list 'default-frame-alist
-                                     '(font . "Monaco-16"))
-                        ))
   )
+
+(use-package modus-themes
+  :ensure
+  :init
+  ;; Add all your customizations prior to loading the themes
+  (setq modus-themes-slanted-constructs t
+        modus-themes-bold-constructs t
+        modus-themes-syntax nil
+        modus-themes-line '3d
+        modus-themes-subtle-line-numbers t
+        modus-themes-paren-match 'subtle-bold
+        )
+
+  ;; Load the theme files before enabling a theme
+  (modus-themes-load-themes)
+  :bind ("<f5>" . modus-themes-toggle))
+
+(add-hook 'after-init-hook
+          (lambda ()
+            ;; load spacemacs-dark
+            ;; (load-theme 'spacemacs-dark t)
+            ;; load  modus-vivendi
+            (modus-themes-load-vivendi)
+            (add-to-list 'default-frame-alist
+                         '(font . "Monaco-16"))
+            ))
+
 (provide 'init-themes)
 ;;; init-themes.el ends here
