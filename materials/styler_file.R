d5e48929023f03452419034ef393a4ce45efe0b4
beyondpie <panshi.pie@gmail.com>
2021-09-01 08:39:16 -0700

    use strict = TRUE in styler_file.R

--- materials/styler_file.R
+++ materials/styler_file.R
@@ -5,7 +5,7 @@ if (length(args) != 1) {
 }
 
 ## https://styler.r-lib.org/reference/tidyverse_style.html
-styler::style_file(path = args[1], strict = FALSE,
+styler::style_file(path = args[1], strict = TRUE,
   scope = "indention",
   indent_by = 2,
   start_comments_with_one_space = FALSE)
