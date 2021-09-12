e61489aae670bcf2adc0099217a1a68dc57250c5
beyondpie <panshi.pie@gmail.com>
2021-09-12 16:40:33 -0700

    delete doom-line config

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -33,26 +33,6 @@
   :delight
 )
 
-;; doom-mode line
-;; (use-package doom-modeline
-;;   :ensure t
-;;   :init
-;;   (when (display-graphic-p)
-;;     (setq doom-modeline-icon t)
-;;     (setq doom-modeline-hud t)
-;;     (setq doom-modeline-window-width-limit fill-column)
-;;     (setq doom-modeline-buffer-file-name-style 'relative-from-project)
-;;     (setq doom-modeline-height 20)
-;;     (setq doom-modeline-checker-simple-format nil)
-;;     (setq doom-modeline-buffer-encoding nil)
-;;     (setq doom-modeline-lsp nil)
-;;     (setq doom-modeline-gnus-timer -1)
-;;     (setq doom-modeline-env-version nil)
-;;     (setq doom-modeline-env-enable-python nil)
-;;     (setq doom-modeline-env-python-executable "")
-;;     )
-;;   (doom-modeline-mode 1)
-;;   )
 
 ;; for mark
 (global-set-key (kbd "C-SPC") 'set-mark-command)
