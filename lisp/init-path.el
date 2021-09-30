fa8f49c1c2bcd32eb6949fe0709b22bb3c9fce41
beyondpie <panshi.pie@gmail.com>
2021-09-30 16:04:42 -0700

    add LDFLAGS to shell variables

--- lisp/init-path.el
+++ lisp/init-path.el
@@ -10,7 +10,9 @@
   (setq exec-path-from-shell-arguments nil)
   :config
   ;; for conda env in python-mode
+  (setq exec-path-from-shell-variables '("PATH" "MANPATH"))
   (add-to-list 'exec-path-from-shell-variables "WORKON_HOME" t)
+  (add-to-list 'exec-path-from-shell-variables "LDFLAGS" t)
   (exec-path-from-shell-initialize)
   )
 
