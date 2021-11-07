878263322ce2e55c85cbc1b1c0af243baa52a9f2
beyondpie <panshi.pie@gmail.com>
2021-11-07 00:05:01 -0700

    add hide mode line mode

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -89,7 +89,7 @@
   (advice-add 'describe-function-1 :after #'elisp-demos-advice-describe-function-1)
   )
 
-(defun simplify-minibatch-emacs ()
+(defun beyondpie/simplify-minibatch-emacs ()
   (interactive)
   (use-package emacs
     :delight
@@ -118,5 +118,29 @@
 ;; downcase-region
 (global-unset-key (kbd "C-x C-l"))
 
+;; hide/show modeline
+;; Ref: https://bzg.fr/en/emacs-hide-mode-line/
+(defvar-local hidden-mode-line-mode nil)
+(define-minor-mode hidden-mode-line-mode
+  "Minor mode to hide the mode-line in the current buffer."
+  :init-value nil
+  :global t
+  :variable hidden-mode-line-mode
+  :group 'editing-basics
+  (if hidden-mode-line-mode
+      (setq hide-mode-line mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format hide-mode-line
+          hide-mode-line nil))
+  (force-mode-line-update)
+  ;; Apparently force-mode-line-update is not always enough to
+  ;; redisplay the mode-line
+  (redraw-display)
+  (when (and (called-interactively-p 'interactive)
+             hidden-mode-line-mode)
+    (run-with-idle-timer
+     0 nil 'message
+     (concat "Hidden Mode Line Mode enabled.  "
+             "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
 (provide 'init-utils)
 ;;; init-utils.el ends here
