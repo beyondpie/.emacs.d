40125fa638537b2a4263a7b49c55872d8e9d927b
beyondpie <panshi.pie@gmail.com>
2022-01-14 09:53:14 -0800

    minor

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,7 +3,14 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq org-agenda-files '("~/Dropbox/plan/general.org"))
+(setq org-agenda-files '("~/Dropbox/plan/research.org"))
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+(setq org-adapt-indentation t
+      org-hide-leading-stars t
+      org-old-levels-only t)
 
 (provide 'init-org)
 ;;; init-org.el ends here
