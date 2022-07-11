50c7afef08454420ea2d2dcc4bd06a07c6db8332
beyondpie <panshi.pie@gmail.com>
2022-07-11 12:50:54 -0700

    remove dashboard configuration

--- lisp/init-const.el
+++ lisp/init-const.el
@@ -8,11 +8,6 @@
 	"https://github.com/beyondpie"
 	"My Github page.")
 
-;; figures
-;; (defconst mybanner
-;; 	(expand-file-name "materials/boston_autumn.png" user-emacs-directory)
-;; 	"Banner for dashboard.")
-
 (defconst beyondpie/normal-leader-key
   "SPC"
   "Evil normal state leader key used as a global leader key."
