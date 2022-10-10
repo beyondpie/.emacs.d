4581f92a6380d539d099f2c3e853dc04491ecab6
beyondpie <panshi.pie@gmail.com>
2022-10-10 14:36:10 -0700

    now use pdf-tools from official webiste instead of mine using straight

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -84,8 +84,8 @@
 
 (use-package pdf-tools
   :ensure t
-  :straight (pdf-tools :type git :host github :repo "politza/pdf-tools"
-                      :fork (:host github :repo "beyondpie/pdf-tools"))
+  ;; :straight (pdf-tools :type git :host github :repo "politza/pdf-tools"
+                      ;; :fork (:host github :repo "beyondpie/pdf-tools"))
   :mode ("\\.pdf\\'" . pdf-view-mode)
   :config
   (pdf-tools-install)
