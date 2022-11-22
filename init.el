399fa1c0b3ac239b38b25a83ce171b47d156875a
beyondpie <panshi.pie@gmail.com>
2022-11-21 16:09:40 -0800

    remove straight itself

--- init.el
+++ init.el
@@ -26,21 +26,6 @@
 ;; close the warnings of gccemacs when compiling packages.
 (setq native-comp-async-report-warnings-errors nil)
 
-;; use straight
-;; https://github.com/raxod502/straight.el
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
 ;; stop use backup files
 (setq make-backup-files nil)
 
