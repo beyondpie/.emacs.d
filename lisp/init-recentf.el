e4021a2affbd7f52f37448323bd2d09d08d25495
beyondpie <panshi.pie@gmail.com>
2023-02-05 12:10:30 -0800

    Minor: set C-x C-r as global key for recentf

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -2,10 +2,14 @@
 ;;; Commentary: from purcell
 ;;; Code:
 
+(require 'recentf)
+(global-set-key (kbd "C-x C-r") 'recentf)
+
 (add-hook 'after-init-hook 'recentf-mode)
 (setq-default
  recentf-max-saved-items 1000
  recentf-exclude `("/tmp/" "/ssh:" ,(concat package-user-dir "/.*-autoloads\\.el\\'")))
+
 
 
 (provide 'init-recentf)
