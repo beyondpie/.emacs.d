db7b842d4e66e850c0a2d4642070a15da349779e
beyondpie <panshi.pie@gmail.com>
2023-04-28 10:12:02 -0700

    minor: reduce gc cons threshold to 16M

--- init.el
+++ init.el
@@ -16,8 +16,8 @@
 (setq process-adaptive-read-buffering nil)
 
 ;; garbage collection during startup
-(let ((normal-gc-cons-threshold (* 20 1024 1024))
-      (init-gc-cons-threshold (* 128 1024 1024)))
+(let ((normal-gc-cons-threshold (* 16 1024 1024))
+      (init-gc-cons-threshold (* 32 1024 1024)))
   (setq gc-cons-threshold init-gc-cons-threshold)
   (add-hook 'emacs-startup-hook
             (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
