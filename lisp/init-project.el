04bfd7bfcc12c5e62bc0fd4624d531a9b2a87531
beyondpie <panshi.pie@gmail.com>
2023-08-09 09:43:22 -0700

    remove eglot due to its lack of functions; add find-file package

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -4,7 +4,8 @@
 ;;; Commentary:
 ;; Use project.el directly supported by Emacs 28
 ;; Keep SPC-j-p as project-switch-project key bindings
-
+;; Find files with the package below
+;; https://github.com/redguardtoo/find-file-in-project
 ;;; Code:
 
 (general-define-key
@@ -20,6 +21,12 @@
  "ps" '(project-shell :which-key "project shell")
  "pq" '(project-find-regexp :which-key "project regrex search")
  )
+
+
+(use-package find-file-in-project
+  :init (slot/vc-install :fetcher "github" :repo "redguardtoo/find-file-in-project")
+  
+  )
 
 (provide 'init-project)
 ;;; init-project.el ends here
