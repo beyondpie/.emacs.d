371f8cf16a28d7d941027271384b680bf87585b3
beyondpie <panshi.pie@gmail.com>
2023-09-04 23:29:33 -0700

    minor: add back R program in mac and remove flymake config in ESS

--- lisp/init-ess.el
+++ lisp/init-ess.el
@@ -19,22 +19,22 @@
   ;; ESS highlighting
   ;; https://emacs.stackexchange.com/questions/60924/how-to-add-function-call-highlighting-in-ess
   (setq ess-R-font-lock-keywords
-      '((ess-R-fl-keyword:keywords   . t)
-        (ess-R-fl-keyword:constants  . t)
-        (ess-R-fl-keyword:modifiers  . t)
-        (ess-R-fl-keyword:fun-defs   . t)
-        (ess-R-fl-keyword:assign-ops . t)
-        (ess-R-fl-keyword:%op%       . t)
-        (ess-R-fl-keyword:F&T . t)
-        (ess-R-fl-keyword:F&T . t)
-        (ess-R-fl-keyword:messages . t)
-        (ess-fl-keyword:fun-calls    . t)
-        (ess-fl-keyword:numbers . t)
-        (ess-fl-keyword:operators . t)
-        (ess-fl-keyword:delimiters . t)
-        (ess-fl-keyword:= . t)
-        (ess-fl-keyword:matrix-labels . t)
-        ))
+        '((ess-R-fl-keyword:keywords   . t)
+          (ess-R-fl-keyword:constants  . t)
+          (ess-R-fl-keyword:modifiers  . t)
+          (ess-R-fl-keyword:fun-defs   . t)
+          (ess-R-fl-keyword:assign-ops . t)
+          (ess-R-fl-keyword:%op%       . t)
+          (ess-R-fl-keyword:F&T . t)
+          (ess-R-fl-keyword:F&T . t)
+          (ess-R-fl-keyword:messages . t)
+          (ess-fl-keyword:fun-calls    . t)
+          (ess-fl-keyword:numbers . t)
+          (ess-fl-keyword:operators . t)
+          (ess-fl-keyword:delimiters . t)
+          (ess-fl-keyword:= . t)
+          (ess-fl-keyword:matrix-labels . t)
+          ))
   :config
   (setq ess-fl-keyword:numbers
         (cons "\\b\\.?[0-9]+[.eEL]?[0-9]*\\b\\|\\(\\sw+\\)[:@\$]+" 'ess-numbers-face))
@@ -51,6 +51,8 @@
    ess-use-flymake t
    ess-startup-directory 'default-directory
    )
+  (setq tree-sitter-mode t)
+  (setq tree-sitter-hl-mode nil)
   :general
   (:states '(normal visual)
            :keymaps 'ess-r-mode-map
@@ -86,19 +88,55 @@
            "sk" '(outline-show-branches :which-key "show branches")
            "ss" '(outline-show-subtree :which-key "show subtree")
            )
-  ;; (when *is-a-mac*
-  ;;   (setq inferior-R-program "/usr/local/bin/R"))
-  )
+  (when *is-a-mac*
+    (setq inferior-R-program "/usr/local/bin/R"))
+  :config
+  ;; ref: https://github.com/zhenhua-wang/emacs.d/blob/a045e49faf8f80e1447721726fc1b17b0744051f/lisp/zw-company.el#L94
+  (defun company-R-objects--prefix ()
+    (unless (ess-inside-string-or-comment-p)
+      (let ((start (ess-symbol-start)))
+        (when start
+          (buffer-substring-no-properties start (point))))))
 
-(defun remoteR (&optional start-args)
-  "Start R REPL remotely.
-  FIXME: how to avoid exploring the conda path we have.
-  "
-  (interactive "P")
-  (let ((inferior-R-program-name "/home/szu/mambaforge/envs/seurat/bin/R"))
-      (set-buffer (run-ess-r start-args)))
-  )
+  (defun company-R-objects--candidates (arg)
+    (let ((proc (ess-get-next-available-process)))
+      (when proc
+        (with-current-buffer (process-buffer proc)
+          (all-completions arg (ess--get-cached-completions arg))))))
 
+  (defun company-capf-with-R-objects--check-prefix (prefix)
+    (cl-search "$" prefix))
+
+  (defun company-capf-with-R-objects (command &optional arg &rest ignored)
+    (interactive (list 'interactive))
+    (cl-case command
+      (interactive (company-begin-backend 'company-R-objects))
+      (prefix (company-R-objects--prefix))
+      (candidates (if (company-capf-with-R-objects--check-prefix arg)
+                      (company-R-objects--candidates arg)
+                    (company-capf command arg)))
+      (annotation (if (company-capf-with-R-objects--check-prefix arg)
+                      "R-object"
+                    (company-capf command arg)))
+      (kind (if (company-capf-with-R-objects--check-prefix arg)
+                'field
+              (company-capf command arg)))
+      (doc-buffer (company-capf command arg))))
+
+  (add-hook 'ess-r-mode-hook
+            (lambda ()
+              (setq-local company-backends
+                          '(company-files company-capf-with-R-objects))))
+  (add-hook 'inferior-ess-r-mode-hook
+            (lambda ()
+              (setq-local company-backends
+                          '(company-files company-R-library company-R-objects))))
+  :config
+  (defun setTrampR ()
+    (interactive)
+    (setq-local inferior-ess-r-program "/home/szu/mambaforge/envs/seurat/bin/R")
+    )
+  )
 
 (provide 'init-ess)
 ;;; init-ess.el ends here
