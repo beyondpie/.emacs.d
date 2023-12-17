e61eda4cde7b83eafa718ec2603c50ab98c7598a
beyondpie <panshi.pie@gmail.com>
2023-12-16 16:17:39 -0800

    add codes in init-macos.el to replace exec-path-from-shell

--- lisp/init-macos.el
+++ lisp/init-macos.el
@@ -63,7 +63,53 @@
    "Use \"trash\" to move FILE to the system trash."
    (cl-assert (executable-find  "trash") nil
               "' trash ' must be installed. Needs \"brew install trash\"")
-  (call-process  "trash" nil 0 nil  "-F"  file))
+   (call-process  "trash" nil 0 nil  "-F"  file))
+
+;; https://www.reddit.com/r/emacs/comments/f8xwau/hack_replace_execpathfromshell/
+;;; Code to replace exec-path-from-shell
+;; Need to create file in $HOME/.emacs.d/.local/env
+;; use this command to create the file  `printenv > $HOME/.emacs.d/.local/env'
+(defconst my-local-dir (concat user-emacs-directory ".local/"))
+(defconst my-env-file (concat my-local-dir "env"))
+(defun my-load-envvars-file (file &optional noerror)
+  "Read and set envvars from FILE.
+If NOERROR is non-nil, don't throw an error if the file doesn't exist or is
+unreadable. Returns the names of envvars that were changed."
+  (if (not (file-readable-p file))
+      (unless noerror
+        (signal 'file-error (list "Couldn't read envvar file" file)))
+    (let (envvars environment)
+      (with-temp-buffer
+        (save-excursion
+          (insert "\n")
+          (insert-file-contents file))
+        (while (re-search-forward "\n *\\([^#= \n]*\\)=" nil t)
+          (push (match-string 1) envvars)
+          (push (buffer-substring
+                 (match-beginning 1)
+                 (1- (or (save-excursion
+                           (when (re-search-forward "^\\([^= ]+\\)=" nil t)
+                             (line-beginning-position)))
+                         (point-max))))
+                environment)))
+      (when environment
+        (setq process-environment
+              (append (nreverse environment) process-environment)
+              exec-path
+              (if (member "PATH" envvars)
+                  (append (split-string (getenv "PATH") path-separator t)
+                          (list exec-directory))
+                exec-path)
+              shell-file-name
+              (if (member "SHELL" envvars)
+                  (or (getenv "SHELL") shell-file-name)
+                shell-file-name))
+        envvars))))
+
+(when (and (or (display-graphic-p)
+               (daemonp))
+           (file-exists-p my-env-file))
+  (my-load-envvars-file my-env-file))
 
 (provide 'init-macos)
 ;;; init-macos.el ends here
