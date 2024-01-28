8bf25fc9d0ee9ef79e69fbc78b9517f9f8c880ed
beyondpie <panshi.pie@gmail.com>
2024-01-28 13:36:15 -0800

    add async dired mode and use rsync for copy

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -3,7 +3,28 @@
 ;; Ref: Purcell
 ;;; Code:
 (setq dired-listing-switches "-aBhl --group-directories-first")
+;; https://www.emacs.dyerdwelling.family/emacs/20240120084016-emacs--dired-async-mode/
+(defun my/rsync (dest)
+  "Rsync copy."
+  (interactive
+    (list
+      (expand-file-name (read-file-name "rsync to:"
+                          (dired-dwim-target-directory)))))
+  (let ((files (dired-get-marked-files nil current-prefix-arg))
+         (command "rsync -arvz --progress --no-g "))
+    (dolist (file files)
+      (setq command (concat command (shell-quote-argument file) " ")))
+    (setq command (concat command (shell-quote-argument dest)))
+    (async-shell-command command "*rsync*")
+    (dired-unmark-all-marks)
+    (other-window 1)
+    (sleep-for 1)
+    (dired-revert)
+    (revert-buffer nil t nil)))
+
+
 (with-eval-after-load 'dired
+  (dired-async-mode 1)
   (setq-default dired-dwim-target t)
   ;; https://emacs-china.org/t/emacs/23850/8
   (add-hook 'dired-mode-hook #'dired-hide-details-mode)
@@ -21,7 +42,7 @@
    "g" 'revert-buffer
    "u" 'dired-unmark
    "+" 'dired-create-directory
-   "C" 'dired-do-copy
+   "C" 'my/rsync
    "R" 'dired-do-rename
    "(" 'dired-hide-details-mode
    "!" 'dired-do-shell-command
