676ca4e37fafda86105c4af827ac8e2aadce78fb
beyondpie <panshi.pie@gmail.com>
2024-09-09 14:51:28 -0700

    remove mediaotr/tscc2 programs like python or R

--- lisp/init-const.el
+++ lisp/init-const.el
@@ -43,38 +43,10 @@
   "R REPL default"
   :type 'string
   :group 'beyondpie-program)
-(defcustom encoder-R
-  "/home/szu/mambaforge/envs/seurat/bin/R"
-  "R REPL under TSCC encoder"
-  :type 'string
-  :group 'beyondpie-program)
-
-(defcustom mediator-R
-  "/home/szu/miniforge3/envs/r/bin/R"
-  "R REPL under TSCC mediator"
-  :type 'string
-  :group 'beyondpie-program)
-(defcustom tscc2-ipython
-  "/tscc/nfs/home/szu/miniforge3/envs/sa2/bin/ipython"
-  "ipython REPL in TSCC2"
-  :type 'string
-  :group 'beyondpie-program)
 
 (defcustom mac-ls
   "/usr/local/bin/gls"
   "insert-directory-program in macos for dired"
-  :type 'string
-  :group 'beyondpie-program)
-
-(defcustom encoder-ipython
-  "/home/szu/mambaforge/envs/sa2/bin/ipython"
-  "ipython REPL in TSCC encoder"
-  :type 'string
-  :group 'beyondpie-program)
-
-(defcustom mediator-ipython
-  "/home/szu/miniforge3/envs/sa2/bin/ipython"
-  "ipython REPL in TSCC mediator"
   :type 'string
   :group 'beyondpie-program)
 
@@ -83,12 +55,6 @@
   "flymake command for python"
   :type 'list
   :group 'beyondpie-program)
-
-(defcustom research-agenda
-  "/Users/szu/Library/Mobile Documents/com~apple~CloudDocs/AP/plan/research.org"
-  "research agenda file for org-mode"
-  :type 'string
-  :group 'beyondpie-file)
 
 (provide 'init-const)
 ;;; init-const.el ends here
