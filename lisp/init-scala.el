957783cf73e3cf11ac23e0b0a74cfcdf9f0fecef
beyondpie <panshi.pie@gmail.com>
2024-09-10 10:59:29 -0700

    [REMOVE LSP] remove lsp-mode

--- lisp/init-scala.el
+++ lisp/init-scala.el
@@ -43,10 +43,6 @@
            "sL" '(sbt-send-line :which-key "sbt-send-line")
            "sr" '(my-sbt-send-region :which-key "my-sbt-send-region")
            "sl" '(my-sbt-send-line :which-key "my-sbt-send-line")
-           "gg" '(lsp-find-implementation :which-key "lsp find imp")
-           ;; "rn" '(lsp-rename :which-key "lsp rename")
-           ;; "rf" '(lsp-format-region :which-key "lsp format")
-           ;; "rb" '(lsp-format-buffer :which-key "lsp format buffer")
            )
   :config
   (defun my-sbt:send-region (start end)
@@ -94,26 +90,6 @@
     (sbt:send-region (line-beginning-position)
                      (line-end-position)))
   )
-
-;; (use-package lsp-metals
-;;   :ensure t
-;;   :custom
-;;   ;; You might set metals server options via -J arguments. This might not always work, for instance when
-;;   ;; metals is installed using nix. In this case you can use JAVA_TOOL_OPTIONS environment variable.
-;;   (lsp-metals-server-args '(;; Metals claims to support range formatting by default but it supports range
-;;                             ;; formatting of multiline strings only. You might want to disable it so that
-;;                             ;; emacs can use indentation provided by scala-mode.
-;;                             "-J-Dmetals.allow-multiline-string-formatting=off"
-;;                             ;; Enable unicode icons. But be warned that emacs might not render unicode
-;;                             ;; correctly in all cases.
-;;                             "-J-Dmetals.icons=unicode"))
-;;   ;; In case you want semantic highlighting. This also has to be enabled in lsp-mode using
-;;   ;; `lsp-semantic-tokens-enable' variable. Also you might want to disable highlighting of modifiers
-;;   ;; setting `lsp-semantic-tokens-apply-modifiers' to `nil' because metals sends `abstract' modifier
-;;   ;; which is mapped to `keyword' face.
-;;   (lsp-metals-enable-semantic-highlighting t)
-;;   :hook (scala-mode . lsp)
-;;   )
 
 (provide 'init-scala)
 ;;; init-scala.el ends here
