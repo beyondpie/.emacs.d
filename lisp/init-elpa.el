2b9ef6d088946159d017c5ffecc374ae8658644e
beyondpie <panshi.pie@gmail.com>
2025-02-25 09:10:25 -0800

    use gnu and melpa as resource and remove my-save-selected-packages functions

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -17,20 +17,24 @@
 ;; HACK: DO NOT copy package-selected-packages to init/custom file forcibly.
 ;; https://github.com/jwiegley/use-package/issues/383#issuecomment-247801751
 ;; from seagle
-(defun my-save-selected-packages (&optional value)
-  "Set `package-selected-packages' to VALUE but don't save to `custom-file'."
-  (when value
-    (setq package-selected-packages value)))
-(advice-add 'package--save-selected-packages
-            :override #'my-save-selected-packages)
+;; (defun my-save-selected-packages (&optional value)
+;;   "Set `package-selected-packages' to VALUE but don't save to `custom-file'."
+;;   (when value
+;;     (setq package-selected-packages value)))
+;; (advice-add 'package--save-selected-packages
+;;             :override #'my-save-selected-packages)
+
+;; (setq package-archives
+;;       '(("gnu"   . "https://elpa.gnu.org/packages/")
+;;         ("melpa" . "https://melpa.org/packages/")
+;;         ("melpa-stable" . "https://stable.melpa.org/packages/")
+;; 	      ("nongnu" . "https://elpa.nongnu.org/nongnu/"))
+;;       )
 
 (setq package-archives
       '(("gnu"   . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")
-	      ("nongnu" . "https://elpa.nongnu.org/nongnu/"))
-      )
-
+      ))
 ;; Initialize packages
 (unless (bound-and-true-p package--initialized) ; To avoid warnings in 27
    (setq package-enable-at-startup nil)          ; To prevent initializing twice
