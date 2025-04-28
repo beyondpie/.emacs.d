d997dac8c82d542d51f173612b357a6d02e55479
beyondpie <panshi.pie@gmail.com>
2025-04-28 12:51:56 -0700

    add setgui function back

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -69,5 +69,64 @@ ALPHA is numeric value from [0, 100]."
 ;; (advice-remove 'ef-themes-select #'update-evil-cursor)
 ;; (advice-remove 'ef-themes-select #'beyondpie/set-evil-insert-state-cursor)
 
+;; http://xahlee.info/emacs/emacs/emacs_customize_default_window_size.html
+(defun beyondpie/setgui ()
+  (interactive)
+  (setq use-file-dialog nil)
+  (setq use-dialog-box nil)
+  (setq-default
+   window-resize-pixelwise t
+   frame-resize-pixelwise t)
+
+  (scroll-bar-mode -1)
+  (defun my/disable-scroll-bars (frame)
+    (modify-frame-parameters frame
+                             '((vertical-scroll-bars . nil)
+                               (horizontal-scroll-bars . nil))))
+  (add-hook 'after-make-frame-functions 'my/disable-scroll-bars)
+
+  (tool-bar-mode -1)
+  (menu-bar-mode -1)
+  (scroll-bar-mode -1)
+
+  (let ((no-border '(internal-border-width . 0)))
+    (add-to-list 'default-frame-alist no-border)
+    (add-to-list 'initial-frame-alist no-border))
+
+  (setq frame-title-format
+        '((:eval (if (buffer-file-name)
+                     (abbreviate-file-name (buffer-file-name))
+                   "%b"))))
+  (progn
+    (set-face-attribute 'default nil :font "Monaco-16")
+    (setq initial-frame-alist '( (tool-bar-lines . 0)))
+    (setq default-frame-alist '( (tool-bar-lines . 0)))
+    )
+
+  ;; Non-zero values for `line-spacing' can mess up ansi-term and co,
+  ;; so we zero it explicitly in those cases.
+  (add-hook 'term-mode-hook
+            (lambda ()
+              (setq line-spacing 0)))
+  (if (display-graphic-p)
+      (progn
+        (set-face-attribute 'default nil :font "Monaco-16")
+        (setq initial-frame-alist
+              '(
+                (tool-bar-lines . 0)
+                (width . 106) ; chars
+                (height . 60) ; lines
+                (ns-transparent-titlebar . t)
+                ))
+        (setq default-frame-alist
+              '(
+                (tool-bar-lines . 0)
+                (width . 106)
+                (height . 60)
+                (ns-transparent-titlebar . t)
+                )))
+    )
+  (beyondpie/set-evil-insert-state-cursor)
+  )
 (provide 'init-themes)
 ;;; init-themes.el ends here
