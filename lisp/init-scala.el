df15e85de5748a37e92e1c115907ad3c0f4c9485
beyondpie <panshi.pie@gmail.com>
2025-05-15 10:41:08 -0700

    Major: use lsp and lsp-metals for scala instead of eglot

--- lisp/init-scala.el
+++ lisp/init-scala.el
@@ -31,6 +31,64 @@
   (lambda ()
     (setq-local left-margin-width nil)))
 
+(use-package lsp-mode
+  :delight
+  :init
+  (setq lsp-keymap-prefix "C-c l"
+        lsp-enable-symbol-highlighting t
+        lsp-lens-enable t
+	      lsp-keep-workspace-alive t
+        lsp-enable-snippet t
+        lsp-enable-xref t
+	      lsp-signature-auto-activate t
+        lsp-signature-render-documentation t
+	      lsp-modeline-code-actions-enable t
+        lsp-diagnostics-provider :flymake
+	      lsp-modeline-diagnostics-enable t
+	      lsp-modeline-workspace-status-enable nil
+        ;; https://emacs-china.org/t/spacemacs-c-c/15695
+        lsp-headerline-breadcrumb-enable nil
+        lsp-completion-show-detail t
+        lsp-completion-show-kind t
+        ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+	      lsp-enable-file-watchers nil
+	      lsp-enable-folding t
+        lsp-semantic-tokens-enable t
+	      lsp-enable-symbol-highlighting t
+	      lsp-enable-text-document-color t
+        lsp-enable-imenu t
+        lsp-eldoc-enable-hover t
+	      lsp-enable-indentation t
+	      lsp-enable-on-type-formatting nil
+        lsp-idle-delay 0.200
+        lsp-log-io nil
+        lsp-modeline-code-actions-enable nil
+        )
+  :hook
+  (lsp-mode . lsp-enable-which-key-integration)
+  :commands
+  lsp
+  )
+(use-package lsp-metals
+  :ensure t
+  :custom
+  ;; You might set metals server options via -J arguments. This might not always work, for instance when
+  ;; metals is installed using nix. In this case you can use JAVA_TOOL_OPTIONS environment variable.
+  (lsp-metals-server-args '(;; Metals claims to support range formatting by default but it supports range
+                            ;; formatting of multiline strings only. You might want to disable it so that
+                            ;; emacs can use indentation provided by scala-mode.
+                            "-J-Dmetals.allow-multiline-string-formatting=off"
+                            ;; Enable unicode icons. But be warned that emacs might not render unicode
+                            ;; correctly in all cases.
+                            "-J-Dmetals.icons=unicode"))
+  ;; In case you want semantic highlighting. This also has to be enabled in lsp-mode using
+  ;; `lsp-semantic-tokens-enable' variable. Also you might want to disable highlighting of modifiers
+  ;; setting `lsp-semantic-tokens-apply-modifiers' to `nil' because metals sends `abstract' modifier
+  ;; which is mapped to `keyword' face.
+  (lsp-metals-enable-semantic-highlighting t)
+  :hook (scala-mode . lsp)
+  )
+
 (use-package scala-mode
   :init
   (slot/vc-install :fetcher "github"
@@ -40,7 +98,7 @@
   ;;      scala-indent:align-forms t)
   :interpreter ("scala" . scala-mode)
   :hook (
-         (scala-mode . eglot-ensure)
+         ;; (scala-mode . eglot-ensure)
          ;;(scala-mode . tree-sitter-hl-mode)
          (scala-mode . company-mode)
          (scala-mode . my-buffer-face-mode-fixed)
@@ -56,6 +114,10 @@
            "sL" '(sbt-send-line :which-key "sbt-send-line")
            "sr" '(my-sbt-send-region :which-key "my-sbt-send-region")
            "sl" '(my-sbt-send-line :which-key "my-sbt-send-line")
+           "rn" '(lsp-rename :which-key "lsp-rename")
+           "rb" '(lsp-format-buffer :which-key "lsp-format-buffer")
+           "rr" '(lsp-format-region :which-key "lsp-format-region")
+           "gg" '(lsp-goto-type-definition :which-key "lsp-go-type-defi")
            )
   :config
   (defun my-sbt:send-region (start end)
