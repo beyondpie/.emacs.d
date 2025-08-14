b738ac133b785d2131bc820e6738a7e435e9472f
beyondpie <panshi.pie@gmail.com>
2025-08-14 21:48:39 +0800

    move elpa-mirror.el and sh to myelpa repo

--- site-lisp/elpa-mirror/elpa-mirror.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ~/.emacs.d/myelpa && \
-    /Applications/Emacs.app/Contents/MacOS/emacs-nw --batch -l ~/.emacs.d/init.el -l ~/.emacs.d/site-lisp/elpa-mirror/elpa-mirror.el \
-    --eval='(setq elpamr-default-output-directory "~/.emacs.d/myelpa")' \
-    --eval='(elpamr-create-mirror-for-installed)'
