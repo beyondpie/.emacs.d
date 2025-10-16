c3936a862bddc4f61326b3a7852fd8c2394f0aaf
beyondpie <panshi.pie@gmail.com>
2025-10-16 13:39:58 +0800

    enable more lsp configures in scala

--- lisp/init-scala.el
+++ lisp/init-scala.el
@@ -35,32 +35,32 @@
   :init
   (setq lsp-keymap-prefix "C-c l"
         lsp-enable-symbol-highlighting t
-        lsp-lens-enable nil
+        lsp-lens-enable t
 	      lsp-keep-workspace-alive nil
         lsp-enable-snippet t
         lsp-enable-xref t
 	      lsp-signature-auto-activate t
-        lsp-signature-render-documentation nil
-	      lsp-modeline-code-actions-enable nil
-        lsp-diagnostics-provider :none
-	      lsp-modeline-diagnostics-enable nil
-	      lsp-modeline-workspace-status-enable nil
+        lsp-signature-render-documentation t
+	      lsp-modeline-code-actions-enable t
+        lsp-diagnostics-provider :flymake
+	      lsp-modeline-diagnostics-enable t
+	      lsp-modeline-workspace-status-enable t
         ;; https://emacs-china.org/t/spacemacs-c-c/15695
         lsp-headerline-breadcrumb-enable nil
-        lsp-completion-show-detail nil
+        lsp-completion-show-detail t
         lsp-completion-show-kind t
         ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
 	      lsp-enable-file-watchers nil
 	      lsp-enable-folding nil
-        lsp-semantic-tokens-enable nil
+        lsp-semantic-tokens-enable t
 	      lsp-enable-text-document-color t
-        lsp-enable-imenu nil
-        lsp-eldoc-enable-hover nil
+        lsp-enable-imenu t
+        lsp-eldoc-enable-hover t
 	      lsp-enable-indentation nil
 	      lsp-enable-on-type-formatting nil
         lsp-idle-delay 0.500
-        lsp-log-io nil
-        lsp-modeline-code-actions-enable nil
+        lsp-log-io t
+        lsp-modeline-code-actions-enable t
         lsp-enable-dap-auto-configure nil
         )
   :hook
